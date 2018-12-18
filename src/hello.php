88c2accea2fe92dce27fb1d79d9541c335a73239
Karan <himekaranguleria@gmail.com>
2018-12-18 16:47:19 +0530

    initial commit

---
+++ src/hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Karan Guleria <himekaranguleria@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// If you don't to add a custom vendor folder, then use the simple class
+// namespace HelloComposer;
+namespace swapkaran\Hellocomposer;
+
+class Hello
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
